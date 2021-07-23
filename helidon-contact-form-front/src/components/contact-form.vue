9ac468fda31f70b71b27fd8435df1225984ce4b1
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-07-23 17:27:24 +0900

    tailwindcss及び、tailwindcss/forms対応

--- helidon-contact-form-front/src/components/contact-form.vue
+++ helidon-contact-form-front/src/components/contact-form.vue
@@ -1,18 +1,33 @@
 <template>
-    <div>
-        <form action="">
-            <input type="text" name="name" id="name" placeholder="お名前を入力してください！">
-            <input type="email" name="email" id="email" placeholder="メールアドレスを入力してね！">
-            <input type="text" name="contact" id="contact" placeholder="お問い合わせ内容を入力してください！">
-            <input type="submit" value="送信">
-        </form>     
-    </div>
+  <div>
+    <form action="" class="flex flex-col">
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="お名前を入力してください！"
+        class="form-input px-4 py-3 rounded-full"
+      />
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="メールアドレスを入力してね！"
+        class="form-input px-4 py-3 rounded-full"
+      />
+      <input
+        type="text"
+        name="contact"
+        id="contact"
+        placeholder="お問い合わせ内容を入力してください！"
+        class="form-input px-4 py-3 rounded-full"
+      />
+      <input type="submit" value="送信" class="form-input px-4 py-3 rounded-full" />
+    </form>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style lang="scss">
-    
 </style>
