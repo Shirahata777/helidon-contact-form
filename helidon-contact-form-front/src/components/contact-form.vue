50bbdd7ca1da55a5a1c9cfd471157833f5e3d2fa
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-07-23 16:56:17 +0900

    first commit

---
+++ helidon-contact-form-front/src/components/contact-form.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <form action="">
+            <input type="text" name="name" id="name">
+            <input type="email" name="email" id="email">
+            <input type="text" name="contact" id="contact">
+            <input type="submit" value="">
+        </form>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    
+</style>
